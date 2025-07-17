42c254ab67eeb3a9d9e5e4b7e1afba0342ec9862
Manthu-Gowda <147706528+Manthu-Gowda@users.noreply.github.com>
2025-07-17 14:50:18 +0530

    updated mongoDB link

--- server/Database/mongoDb.js
+++ server/Database/mongoDb.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/mydatabase');
 
 async function db(){
-   await mongoose.connect('mongodb://localhost:27017/mydatabase');
+   await mongoose.connect('mongodb+srv://kmanthugowda:Manthu@420@nodereactdemo.veoscsu.mongodb.net/');
    console.log('Connected to MongoDB');
 }
 
