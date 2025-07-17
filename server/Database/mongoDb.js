99248804726ca45eb1ae13ce0fc6f58c31661aba
Manthu-Gowda <147706528+Manthu-Gowda@users.noreply.github.com>
2025-07-17 15:02:21 +0530

    updated mongoDB link

--- server/Database/mongoDb.js
+++ server/Database/mongoDb.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/mydatabase');
 
 async function db(){
-   await mongoose.connect('mongodb+srv://kmanthugowda:Manthu@420@nodereactdemo.veoscsu.mongodb.net/');
+   await mongoose.connect('mongodb+srv://kmanthugowda:Manthu%40420@nodereactdemo.veoscsu.mongodb.net/?retryWrites=true&w=majority');
    console.log('Connected to MongoDB');
 }
 
